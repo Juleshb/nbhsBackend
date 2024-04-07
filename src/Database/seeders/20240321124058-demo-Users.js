9fcebda5406b408cafa2ac50c7e94bce1e5f49a3
juleshb <habaruremajules@gmail.com>
2024-04-08 02:52:36 +0530

    recover data

--- src/Database/seeders/20240321124058-demo-Users.js
+++ src/Database/seeders/20240321124058-demo-Users.js
@@ -2,27 +2,84 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Users', [
-    
-    {
-      role: "nurse",
-      firstName: "kiki",
-      lastName: "mimi",
-      email: "jules2@gmail.com",
-      pin: "$2b$10$y0OUkrPsn14g1Ndmf9JTQ.4TEF8cruCyyq5LBHX99G56fs7cJX4Dq",
-      type: null,
-      profile: null,
-      HealthCentre: null,
-      updatedAt: "2024-04-07T17:09:37.422Z",
-      createdAt: "2024-04-07T17:09:37.422Z"
-    },
-  ], 
-  {}
-  );
+      
+      
+      
+      
+      {
+        role: "admin",
+        firstName: "Jules",
+        lastName: "Habarurema",
+        email: "habaruremajules@gmail.com",
+        pin: "$2b$10$jjwB3xKySnGL4shfJkNmseYb7wReZfpDuwMuEpB0X49jMtPNfJ26i",
+        type: null,
+        profile: null,
+        HealthCentre: null,
+        updatedAt: "2024-03-16T09:49:54.342Z",
+        createdAt: "2024-03-16T09:49:54.342Z"
+      },
+      
+     
+      {
+        role: "admin",
+        firstName: "Olivier ",
+        lastName: "Sibomana ",
+        email: "oliviersibomana917@gmail.com",
+        pin: "$2b$10$fSiZrGDXmMf7KzQB/Vm2G.F63C5uXPw6Ti6bfP0UYpjL0JsYPPNQ6",
+        type: null,
+        profile: null,
+        HealthCentre: null,
+        updatedAt: "2024-03-16T11:06:19.139Z",
+        createdAt: "2024-03-16T11:06:19.139Z"
+      },
+     
+      
+      {
+        role: "ideologist",
+        firstName: "John",
+        lastName: "Bukuru",
+        email: "johnbkrdr@gmail.com",
+        pin: "$2b$10$xtvwPCrP9pW8a9IT9TrKi.xTH2VNgc4MhjSGdp/7kamqvuc45N7sm",
+        type: null,
+        profile: null,
+        HealthCentre: null,
+        updatedAt: "2024-03-17T15:42:19.536Z",
+        createdAt: "2024-03-17T15:42:19.536Z"
+      },
+     
+      
+      {
+        role: "ideologist",
+        firstName: "Bosco",
+        lastName: "Niko",
+        email: "jbosconik@gmai.com",
+        pin: "$2b$10$.V8Olq7iprtI7DP2ffacFeomv2Q6/YCaxRjdFjcGkz.FknkvKLfp6",
+        type: null,
+        profile: null,
+        HealthCentre: null,
+        updatedAt: "2024-03-18T07:48:25.645Z",
+        createdAt: "2024-03-18T07:48:25.645Z"
+      },
+      
+      {
+        role: "ideologist",
+        firstName: "Niko",
+        lastName: "Bosco",
+        email: "jbosconik@gmail.com",
+        pin: "$2b$10$pviSfC1SqGEWIDLmHqgnNeomiJJB5kJMrYPA.j6vPFipjRPq2jZe.",
+        type: null,
+        profile: null,
+        HealthCentre: null,
+        updatedAt: "2024-03-18T07:48:26.851Z",
+        createdAt: "2024-03-18T07:48:26.851Z"
+      },
+      
+    ], {});
   },
 
-  async down (queryInterface, Sequelize) {
-
+  async down(queryInterface, Sequelize) {
+    // Revert the data
   }
 };
